5b37baa3c2faff4734178f402782b9650f4f575b
Matvey <metrloopod2121@mail.ru>
2024-11-18 20:11:30 +0300

    Initial Commit

---
+++ testTodo/testTodoApp.swift
@@ -0,0 +1,20 @@
+//
+//  testTodoApp.swift
+//  testTodo
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct testTodoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ testTodoTests/testTodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTodoTests.swift
+//  testTodoTests
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 18.11.2024.
+//
+
+import Testing
+@testable import testTodo
+
+struct testTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
